6eda613c08ab263b54681c9177e33160e980d7c1
thericcer <thericcer@gmail.com>
2016-01-05 16:47:19 +0000

    Threads working and seems stable!

--- opencv/threads.cpp
+++ opencv/threads.cpp
@@ -17,6 +17,7 @@ struct line_capture{
 	int hough_min_gap;
 	int min_angle;
 	int max_angle;
+	cv::Vec4i roi;
 	float average_angle;
 	cv::Vec4i average_line;
 	int num_good_lines;
@@ -49,7 +50,7 @@ void * line_thread_routine(void * input) {
 			args->hough_min_length, args->hough_min_gap);
 
 
-	if (args->lines.size() > 150) {
+	if (args->lines.size() > 350) {
 		//printf("thread: %d | Too Many Lines! %d | canny_thresh: %d\n", args->thread, args->lines.size(), args->canny_thresh);
 		pthread_mutex_unlock(&line_mutex[args->thread]);
 		pthread_exit(NULL);
@@ -60,17 +61,25 @@ void * line_thread_routine(void * input) {
 		angle = (atan2(line[1]-line[3], line[0]-line[2])*(180/CV_PI));
 		args->average_angle = (angle + args->average_angle) / 2;
 		if ((angle > args->min_angle) && angle < args->max_angle) {
-			args->num_good_lines++;
-			args->average_line[0] = (line[0] + args->average_line[0]) / 2;
-			args->average_line[1] = (line[1] + args->average_line[1]) / 2;
-			args->average_line[2] = (line[2] + args->average_line[2]) / 2;
-			args->average_line[3] = (line[3] + args->average_line[3]) / 2;
+		//	if (line[0] > args->roi[0] && line[0] < args->roi[2]){
+		//	if (line[1] > args->roi[1] && line[1] < args->roi[3]){
+		//	if (line[2] > args->roi[0] && line[2] < args->roi[2]){
+		//	if (line[3] > args->roi[1] && line[1] < args->roi[3]){
+				args->num_good_lines++;
+				args->average_line[0] = (line[0] + args->average_line[0]) / 2;
+				args->average_line[1] = (line[1] + args->average_line[1]) / 2;
+				args->average_line[2] = (line[2] + args->average_line[2]) / 2;
+				args->average_line[3] = (line[3] + args->average_line[3]) / 2;
+		//	}
+		//	}
+		//	}
+		//	}
 		}
 	}
 	
 	//printf("thread: %d | num_good_lines: %d | canny_thresh: %d | average_angle: %f | average_line: [%d, %d] [%d, %d]\n",
-		args->thread, args->num_good_lines, args->canny_thresh, args->average_angle, args->average_line[0], 
-		args->average_line[1], args->average_line[2], args->average_line[3]); 
+	//	args->thread, args->num_good_lines, args->canny_thresh, args->average_angle, args->average_line[0], 
+	//	args->average_line[1], args->average_line[2], args->average_line[3]); 
 	pthread_mutex_unlock(&line_mutex[args->thread]);
 	pthread_exit(NULL);
 }
@@ -121,7 +130,7 @@ int main(int argc, char ** argv) {
 		// Line Thread
 		if (pthread_mutex_trylock(&line_mutex[0]) == 0) {
 			pthread_attr_init(&attr);
-			pthread_attr_setstacksize(&attr, (sizeof(struct arguments)*2) + args.num_good_lines);
+			pthread_attr_setstacksize(&attr, (sizeof(struct line_capture)*500));
 
 			args.thread = 0;
 			args.raw = raw;
@@ -130,20 +139,25 @@ int main(int argc, char ** argv) {
 			args.hough_min_gap = 10;
 			args.min_angle = 130;
 			args.max_angle = 150;
+			args.roi[0] = 150;
+			args.roi[1] = 0;
+			args.roi[2] = 500;
+			args.roi[3] = 300;
 
-			if (args.num_good_lines < 5) {
+			if (args.lines.size() < 150) {
 				if (args.canny_thresh > 0){
 					args.canny_thresh -= 5;
 				}
-			} else {
+			} 
+			if (args.lines.size() > 350){
 				if (args.canny_thresh < 500) {
 					args.canny_thresh += 5;
  				}
 			}
 
 			line_cap = args;
-			printf("thread: %d | num_good_lines: %d | canny_thresh: %d | average_angle: %f | average_line: [%d, %d] [%d, %d]\n",
-				line_cap.thread, line_cap.num_good_lines, line_cap.canny_thresh, line_cap.average_angle, 
+			printf("thread: %d | num_total_lines: %d |  num_good_lines: %d | canny_thresh: %d | average_angle: %f | average_line: [%d, %d] [%d, %d]\n",
+				line_cap.thread, line_cap.lines.size(), line_cap.num_good_lines, line_cap.canny_thresh, line_cap.average_angle, 
 				line_cap.average_line[0],line_cap.average_line[1],line_cap.average_line[2],line_cap.average_line[3]);
 						
 			average_line = args.average_line;
@@ -162,31 +176,36 @@ int main(int argc, char ** argv) {
 		// Line R Thread
 		if (pthread_mutex_trylock(&line_mutex[1]) == 0) {
 			pthread_attr_init(&attr);
-			pthread_attr_setstacksize(&attr, (sizeof(struct arguments)*2) + args_r.num_good_lines);
+			pthread_attr_setstacksize(&attr, (sizeof(struct line_capture)*500));
 
 			args_r.thread = 1;
 			args_r.raw = raw_r;
 			args_r.hough_thresh = 90;
-			args_r.hough_min_length = 50;
+			args_r.hough_min_length = 150;
 			args_r.hough_min_gap = 10;
-			args_r.min_angle = 0;
-			args_r.max_angle = 360;
+			args_r.min_angle = -20;
+			args_r.max_angle = 20;
+			args_r.roi[0] = 0;
+			args_r.roi[1] = 150;
+			args_r.roi[2] = 719;
+			args_r.roi[3] = 300;
 			pthread_mutex_unlock(&line_mutex[1]);
 			
 
-			if (args_r.num_good_lines < 5) {
+			if (args_r.lines.size() < 10) {
 				if (args_r.canny_thresh > 0){
 					args_r.canny_thresh -=5;
 				}
-			} else {
+			} 
+			if (args_r.lines.size() > 150){
 				if (args_r.canny_thresh < 500) {
 					args_r.canny_thresh += 5;
 				}
 			}
 
 			line_cap_r = args_r;
-			printf("thread: %d | num_good_lines: %d | canny_thresh: %d | average_angle: %f | average_line: [%d, %d] [%d, %d]\n",
-				line_cap_r.thread, line_cap_r.num_good_lines, line_cap_r.canny_thresh, line_cap_r.average_angle, 
+			printf("thread: %d | num_total_lines: %d | num_good_lines: %d | canny_thresh: %d | average_angle: %f | average_line: [%d, %d] [%d, %d]\n",
+				line_cap_r.thread, line_cap_r.lines.size(), line_cap_r.num_good_lines, line_cap_r.canny_thresh, line_cap_r.average_angle, 
 				line_cap_r.average_line[0],line_cap_r.average_line[1],line_cap_r.average_line[2],line_cap_r.average_line[3]);
 			
 			average_line_r = args_r.average_line;
@@ -206,8 +225,8 @@ int main(int argc, char ** argv) {
 
 		cv::imshow("raw", raw);
 
-		cv::line(raw_r, cv::Point(line_cap_r.average_line_r[0], line_cap_r.average_line_r[1]),
-			      cv::Point(aline_cap_r.verage_line_r[2], line_cap_r.average_line_r[3]),
+		cv::line(raw_r, cv::Point(line_cap_r.average_line[0], line_cap_r.average_line[1]),
+			      cv::Point(line_cap_r.average_line[2], line_cap_r.average_line[3]),
 			      cv::Scalar(0, 255, 255), 3, CV_AA);
 
 		cv::imshow("raw_r", raw_r);
